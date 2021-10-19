98cdf48d6ad7bf31da7a07d210cab75af4477a41
Dimitri <dimitri.justeau@gmail.com>
2021-10-19 12:42:28 +1100

    Complete generate_landscape_structure.R to allow multiple structure generation and search strategy choice

--- R/generate_landscape_structure.R
+++ R/generate_landscape_structure.R
@@ -4,7 +4,7 @@
 #'
 #'
 #'
-generate_landscape_structure <- function(targets_str, targets_file, output_prefix, nb_solutions=1, search_strategy="DEFAULT") {
+generate_landscape_structure <- function(targets_str, targets_file, nb_solutions=1, search_strategy="DEFAULT") {
   # Check arguments
   if (missing(targets_str)) {
     if (missing(targets_file)) {
@@ -23,10 +23,34 @@ generate_landscape_structure <- function(targets_str, targets_file, output_prefi
   reader <- .jnew("java.io.StringReader", targets_str)
   solver <- J("solver.LandscapeStructureSolver")$readFromJSON(reader)
   .jcall(solver, "V", "build")
-  struct <- .jcall(solver, "Lsolver/LandscapeStructure;", "findSolution")
-  if (is.null(struct)) {
-    stop("User targets cannot be satisfied")
+  structs_json <- c()
+  switch(search_strategy,
+         DEFAULT = .jcall(solver, "V", "setDefaultSearch"),
+         RANDOM = .jcall(solver, "V", "setRandomSearch"),
+         DOM_OVER_W_DEG = .jcall(solver, "V", "setDomOverWDegSearch"),
+         DOM_OVER_W_DEG_REF = .jcall(solver, "V", "setDomOverWDegRefSearch"),
+         MIN_DOM_LB = .jcall(solver, "V", "setMinDomLBSearch"),
+         MIN_DOM_UB = .jcall(solver, "V", "setMinDomUBSearch"),
+         ACTIVITY_BASED = .jcall(solver, "V", "setActivityBasedSearch"),
+         CONFLICT_HISTORY = .jcall(solver, "V", "setConflictHistorySearch")
+  )
+  start_time = Sys.time()
+  for (i in 1:nb_solutions) {
+    start_sol_time = Sys.time()
+    struct <- .jcall(solver, "Lsolver/LandscapeStructure;", "findSolution")
+    if (is.null(struct)) {
+      if (length(structs_json) == 0) {
+        stop("User targets cannot be satisfied")
+      } else {
+        stop("No more solutions satisfying user targets exist")
+      }
+    }
+    cat("Landscape structure", i, "found in", as.numeric(difftime(Sys.time(), start_sol_time, unit = "s")), "s", sep = " ")
+    sol <- .jcall(struct, "Ljava/lang/String;", "toJSON");
+    structs_json <- append(structs_json, sol)
   }
-  struct_json <- .jcall(struct, "Ljava/lang/String;", "toJSON")
-  return(struct_json)
+  if (nb_solutions > 1) {
+    cat("All landscape structures found in", as.numeric(difftime(Sys.time(), start_time, unit = "s")), "s", sep = " ")
+  }
+  return(structs_json)
 }
